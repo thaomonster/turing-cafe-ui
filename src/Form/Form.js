70d08f9a13d0f38ca369ad6c82dd9804b986d135
Thao Ma <MaThao@gmail.com>
2021-02-17 18:58:39 -0700

    Add basic Form component structure

---
+++ src/Form/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+}
+
+export default Form
