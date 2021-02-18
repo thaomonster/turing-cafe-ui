ffbc2e96ebc872e63a79999c280d74193b84dc41
Thao Ma <MaThao@gmail.com>
2021-02-17 19:00:51 -0700

    Add state props

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -5,8 +5,16 @@ class Form extends Component {
   constructor() {
     super();
     this.state = {
-
+      id: Date.now(),
+      name: '',
+      date: '',
+      time: '',
+      numOfGuest: '',
     }
+  }
+
+  render() {
+    
   }
 }
 
